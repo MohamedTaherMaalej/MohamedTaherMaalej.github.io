7f96162d747ab7a80c2c409ec070f11c1a4b2b97
MohamedTaherMaalej <mohamedtahermaalej@gmail.com>
2026-01-24 04:48:01 +0000

    refactor: simplify page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Footer from "@/components/footer";
 import HeroSection from "@/sections/hero-section";
 
 export default function Page() {
-    return (
-        <main className="max-md:px-4">
-            <HeroSection />
-            <Footer />
-        </main>
-    );
+    return <HeroSection />;
 }
